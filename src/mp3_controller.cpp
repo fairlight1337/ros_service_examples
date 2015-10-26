3e72d821f160a060dce7d9ab0f6d31c421be7c16
Jan Winkler <winkler@cs.uni-bremen.de>
2015-10-26 13:13:42 +0100

    Catkinization, source cleanup, migration to C++11, implicit package rename

--- src/mp3_controller.cpp
+++ src/mp3_controller.cpp
@@ -1,48 +1,91 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, Institute for Artificial Intelligence,
+ *  Universität Bremen.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Institute for Artificial Intelligence,
+ *     Universität Bremen, nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/** \author Jan Winkler */
+
+
+// System
 #include <iostream>
-#include "ros/ros.h"
-#include "ros_service_examples/MP3InventoryService.h"
 
-using namespace std;
+// ROS
+#include <ros/ros.h>
+
+// Local
+#include <ros_service_examples/MP3InventoryService.h>
 
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ros::init(argc, argv, "mp3_inventory_client");
   
-  ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<ros_service_examples::MP3InventoryService>("mp3_inventory_interaction");
+  ros::NodeHandle nh;
+  ros::ServiceClient client = nh.serviceClient<ros_service_examples::MP3InventoryService>("mp3_inventory_interaction");
   
   ros_service_examples::MP3InventoryService srv;
-
-  std::stringstream ss;
-  ss << "album_list";
-  srv.request.request_string = ss.str();
-
+  
+  std::stringstream sts;
+  sts << "album_list";
+  srv.request.request_string = sts.str();
+  
   if(client.call(srv)) {
     ROS_INFO("OK, sent. Here is the answer:");
     ROS_INFO(" - Response string: '%s'", srv.response.response_string.c_str());
-
+    
     if(strcmp(srv.response.response_string.c_str(), "album_list") == 0) {
       ROS_INFO(" - Albums:");
-      for(vector<string>::iterator itStrings = srv.response.list_strings.begin(); itStrings != srv.response.list_strings.end(); itStrings++) {
-        ROS_INFO("   * %s", (*itStrings).c_str());
-
+      
+      for(std::string strString : srv.response.list_strings) {
+        ROS_INFO("   * %s", strString.c_str());
+	
         // Call server for information about this album (i.e. titles).
-        std::stringstream ss;
-        ss << "title_list";
+        std::stringstream sts;
+        sts << "title_list";
         ros_service_examples::MP3InventoryService srv_titles;
-        srv_titles.request.request_string = ss.str();
-        ss.str("");
-        ss << (*itStrings).c_str();
-        srv_titles.request.album = ss.str();
-
+        srv_titles.request.request_string = sts.str();
+        sts.str("");
+        sts << strString.c_str();
+        srv_titles.request.album = sts.str();
+	
         if(client.call(srv_titles)) {
           ROS_INFO("     Titles:");
-
-          for(vector<string>::iterator itTitles = srv_titles.response.list_strings.begin(); itTitles != srv_titles.response.list_strings.end(); itTitles++) {
-            ROS_INFO("       o %s", (*itTitles).c_str());
-          }
+	  
+	  for(std::string strTitle : srv_titles.response.list_strings) {
+            ROS_INFO("       o %s", strTitle.c_str());
+	  }
         } else {
-          ROS_INFO("Failed to retrieve album titles for album '%s'", (*itStrings).c_str());
+          ROS_INFO("Failed to retrieve album titles for album '%s'", strString.c_str());
         }
       }
     } else {
--- src/mp3_inventory.cpp
+++ src/mp3_inventory.cpp
@@ -1,55 +1,99 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, Institute for Artificial Intelligence,
+ *  Universität Bremen.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Institute for Artificial Intelligence,
+ *     Universität Bremen, nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/** \author Jan Winkler */
+
+
+// System
 #include <iostream>
 #include <string>
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
 #include <vector>
-#include "ros/ros.h"
-#include "ros_service_examples/MP3InventoryService.h"
 
-using namespace std;
+// ROS
+#include <ros/ros.h>
+
+// Local
+#include <ros_service_examples/MP3InventoryService.h>
 
 
 bool mp3_inventory_interaction(ros_service_examples::MP3InventoryService::Request &req,
                                ros_service_examples::MP3InventoryService::Response &res) {
-  string strDirectory = "albums";
+  std::string strDirectory = "albums";
   ROS_INFO("request string: %s", req.request_string.c_str());
-
-  if(strcmp(req.request_string.c_str(), "album_list") == 0) {
-    string str_temp;
-    stringstream ss;
-
+  
+  if(std::strcmp(req.request_string.c_str(), "album_list") == 0) {
+    std::string str_temp;
+    std::stringstream sts;
+    
     DIR *dp;
     struct dirent *dirp;
     if((dp = opendir(strDirectory.c_str())) != NULL) {
       while((dirp = readdir(dp)) != NULL) {
-        string strTempFilename = string(dirp->d_name);
-
-        if(strcmp(strTempFilename.c_str(), ".") != 0 && strcmp(strTempFilename.c_str(), "..") != 0) {
-          res.list_strings.push_back(string(dirp->d_name));
+	std::string strTempFilename = std::string(dirp->d_name);
+	
+        if(std::strcmp(strTempFilename.c_str(), ".") != 0 &&
+	   std::strcmp(strTempFilename.c_str(), "..") != 0) {
+          res.list_strings.push_back(std::string(dirp->d_name));
         }
       }
     }
-
-    ss.str("");
-    ss << req.request_string.c_str();
-    res.response_string = ss.str();
-
+    
+    sts.str("");
+    sts << req.request_string.c_str();
+    res.response_string = sts.str();
+    
     ROS_INFO("Sending back %ld album(s).", res.list_strings.size());
-  } else if(strcmp(req.request_string.c_str(), "title_list") == 0) {
-    string strAlbumDirectory = strDirectory + "/" + req.album.c_str();
-
-    string str_temp;
-    stringstream ss;
-
+  } else if(std::strcmp(req.request_string.c_str(), "title_list") == 0) {
+    std::string strAlbumDirectory = strDirectory + "/" + req.album.c_str();
+    
+    std::string str_temp;
+    std::stringstream sts;
+    
     DIR *dp;
     struct dirent *dirp;
     if((dp = opendir(strAlbumDirectory.c_str())) != NULL) {
       while((dirp = readdir(dp)) != NULL) {
-        string strTempFilename = string(dirp->d_name);
-
-        if(strcmp(strTempFilename.c_str(), ".") != 0 && strcmp(strTempFilename.c_str(), "..") != 0) {
-          res.list_strings.push_back(string(dirp->d_name));
+	std::string strTempFilename = std::string(dirp->d_name);
+	
+        if(std::strcmp(strTempFilename.c_str(), ".") != 0 &&
+	   std::strcmp(strTempFilename.c_str(), "..") != 0) {
+          res.list_strings.push_back(std::string(dirp->d_name));
         }
       }
     }
@@ -64,11 +108,11 @@ bool mp3_inventory_interaction(ros_service_examples::MP3InventoryService::Reques
 }
 
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ros::init(argc, argv, "mp3_inventory_node");
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
   
-  ros::ServiceServer service = n.advertiseService("mp3_inventory_interaction", mp3_inventory_interaction);
+  ros::ServiceServer service = nh.advertiseService("mp3_inventory_interaction", mp3_inventory_interaction);
   
   ROS_INFO("MP3 Inventory up and running.");
   ros::spin();
